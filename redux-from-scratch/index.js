1c65e7ed7726345eb8e245d625dd579f69ccb774
Michal Michalczuk <michalczukm@gmail.com>
2017-03-10 00:45:23 +0100

    jquery-redux rxjs redux implementation
    * todo list example

--- redux-from-scratch/index.js
+++ redux-from-scratch/index.js
@@ -3,7 +3,50 @@
 $(() => {
     let $list = $('#todo-list');
 
-    ['1', '2', '3'].forEach((item) => {
-       $('<li/>').text(item).appendTo($list);
+    store$.subscribe((state) => {
+        $list.empty();
+        state.forEach((item) => {
+            $('<li/>').text(item).appendTo($list);
+        });
     });
 });
+
+const addItem = (event) => {
+    let $newItem = $('#new-item');
+
+    let todoItem = $newItem.val();
+    $newItem.val('');
+
+    addItemAction(todoItem);
+};
+
+// actions stream
+const action$ = new Rx.Subject();
+
+// initial state
+const initialState = [];
+
+// reducer
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_ITEM': {
+            return [...state, action.payload];
+        }
+        default:
+            return state;
+    }
+};
+
+// store
+const store$ = action$.startWith(initialState).scan(reducer);
+
+// action dispacher
+const actionDispacher = (func) => (...args) => {
+    action$.next(func(...args));
+};
+
+// actions
+const addItemAction = actionDispacher((payload) => ({
+    type: 'ADD_ITEM',
+    payload
+}));
